3a373eb6c11a474a60d78711800a1dbd071be7a7
Fede Escobar <fescobar_2@hotmail.com>
2024-06-04 22:27:52 -0400

    click dialog

--- sketch.js
+++ sketch.js
@@ -1,3 +1,22 @@
+let img1, img2;
+let dialog = [
+  { speaker: 1, text: "Hello there!" },
+  { speaker: 2, text: "Hi! How are you?" },
+  { speaker: 1, text: "I'm good, thanks. What about you?" },
+  { speaker: 2, text: "Doing great!" }
+];
+let currentDialog = 0;
+
+// function preload() {
+//   img1 = loadImage("./assets/train-og.png");; // replace with your image path
+//   img2 = loadImage("./assets/robot-yellow.png");; // replace with your image path
+// }
+
+// function setup() {
+//   createCanvas(800, 400);
+//   textSize(24);
+// }
+
 // assets
 let backgroundImage;
 let cloud;
@@ -29,12 +48,13 @@ let y = 0;
 let dim = 80.0;
 
 function setup() {
-  createCanvas(626*1.5, 417*1.5);
+  createCanvas(626 * 1.5, 417 * 1.5);
+  textSize(24);
+
   trailPosX2 = width;
   bgPosX2 = width;
   // music.loop();
 }
-
 
 
 // carregando assets
@@ -45,9 +65,32 @@ function preload() {
   train = loadImage("./assets/train-og.png");
   trainShadow = loadImage("./assets/robot-yellow.png");
   // music = loadSound("assets/music.mp3");
+
+  img1 = loadImage("./assets/train-og.png");; // replace with your image path
+  img2 = loadImage("./assets/robot-yellow.png");; // replace with your image path
 }
 
+function drawDialog() {
+  // background(255);
+  // image(img1, 50, 100, 200, 200);
+  // image(img2, 550, 100, 200, 200);
 
+  if (dialog[currentDialog].speaker === 1) {
+    fill(0);
+    rect(35, 50, 220, 50, 10);
+    fill(255);
+    text(dialog[currentDialog].text, 35, 85);
+  } else {
+    fill(0);
+    rect(520, 150, 220, 50, 10);
+    fill(255);
+    text(dialog[currentDialog].text, 520, 180);
+  }
+}
+
+function mousePressed() {
+  currentDialog = (currentDialog + 1) % dialog.length;
+}
 
 //background functions
 function drawBG() {
@@ -58,10 +101,10 @@ function drawBG() {
 function moveBG() {
   bgPosX1 += 1;
   bgPosX2 += 1;
-  
-  if (bgPosX1 >= width){
+
+  if (bgPosX1 >= width) {
     bgPosX1 = -width + 0.1;
-  } if (bgPosX2 >= width){
+  } if (bgPosX2 >= width) {
     bgPosX2 = -width + 0.1;
   }
 }
@@ -70,17 +113,17 @@ function moveBG() {
 
 // trail functions
 function drawTrail() {
-  image(trail, trailPosX1, 50, width/4, height/8);
-  image(trail, trailPosX2, 50, width/4, height/8);
+  image(trail, trailPosX1, 50, width / 4, height / 8);
+  image(trail, trailPosX2, 50, width / 4, height / 8);
 }
 
 function moveTrail() {
   trailPosX1 += trailSpeedX;
   trailPosX2 += trailSpeedX;
-  
-  if (trailPosX1 >= width){
+
+  if (trailPosX1 >= width) {
     trailPosX1 = -width + 10;
-  } if (trailPosX2 >= width){
+  } if (trailPosX2 >= width) {
     trailPosX2 = -width + 10;
   }
 }
@@ -94,7 +137,7 @@ function drawCloud() {
 
 function moveCloud() {
   cloudPosX += cloudSpeedX;
-  
+
   if (cloudPosX >= width) {
     cloudPosX = 0 - 456;
   }
@@ -109,12 +152,12 @@ function drawTrain() {
     x = -dim;
   }
   image(train, trainPosX, trainPosY, 150, 90);
-  translate(x, height / 2 - dim / 2);
+  // translate(x, height / 2 - dim / 2);
   image(trainShadow, 0, 120, 180, 160)
 }
 
 function moveTrain() {
-  trainPosY = 10+ (random(-1, 1));
+  trainPosY = 10 + (random(-1, 1));
   if (trainPosY >= 1) {
     trainPosY = 10;
   } if (trainPosY <= 0) {
@@ -136,8 +179,9 @@ function draw() {
   drawBG();
   moveBG();
   drawTrain();
-  // moveTrain();
-  drawTrail();
+  drawDialog();
+  moveTrain();
+  // drawTrail();
   // moveTrail();
   // playMusic();
 }
